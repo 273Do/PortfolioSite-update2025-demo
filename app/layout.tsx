ef5001ad781ed43224f3f4d8cde3e72d13068992
273* <keita.y310@icloud.com>
2025-06-15 01:04:36 +0900

    ✨ ヘッダーを適応

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import * as Layout from "@/features/layouts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,9 @@ export default function RootLayout({
   return (
     <html lang="ja" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} m-10 antialiased`}
       >
+        <Layout.Header />
         {children}
       </body>
     </html>
