d5858e76c18d9133f34d2572d40ba2faf2d27764
273* <keita.y310@icloud.com>
2025-06-15 23:06:03 +0900

    ✨ 画面のレイアウトコンポーネントを実装

---
+++ components/ContentScreenLayout.tsx
@@ -0,0 +1,12 @@
+export const ContentScreenLayout = async ({
+  children,
+  className,
+}: { children: React.ReactNode; className?: string }) => {
+  return (
+    <div className={`flex h-[calc(100vh-5rem)] items-end border ${className}`}>
+      <div className="flex size-full flex-col items-end justify-between pt-20">
+        {children}
+      </div>
+    </div>
+  );
+};
