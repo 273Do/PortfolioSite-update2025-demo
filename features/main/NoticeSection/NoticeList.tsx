227d2867966c3cc792395596017ef638f6075401
273* <keita.y310@icloud.com>
2025-06-17 22:32:13 +0900

    🩹 不要なコードを削除

--- features/main/NoticeSection/NoticeList.tsx
+++ features/main/NoticeSection/NoticeList.tsx
@@ -52,40 +52,6 @@ const NoticeList = () => {
         </div>
         <p>{selectNotice.description}</p>
       </div>
-      {/* <div className="flex w-1/2 flex-col gap-6">
-        <div>
-          <ul className="flex items-center justify-between">
-            <li>Notice contents here</li>
-            <li>yyyy-mm-dd</li>
-          </ul>
-          <ul className="flex items-center justify-between">
-            <li>Notice contents here</li>
-            <li>yyyy-mm-dd</li>
-          </ul>
-          <ul className="flex items-center justify-between">
-            <li>Notice contents here</li>
-            <li>yyyy-mm-dd</li>
-          </ul>
-          <ul className="flex items-center justify-between">
-            <li>Notice contents here</li>
-            <li>yyyy-mm-dd</li>
-          </ul>
-          <ul className="flex items-center justify-between">
-            <li>Notice contents here</li>
-            <li>yyyy-mm-dd</li>
-          </ul>
-          <ul className="flex items-center justify-between">
-            <li>Notice contents here</li>
-            <li>yyyy-mm-dd</li>
-          </ul>
-        </div>
-        <p>
-          I am someone who enjoys making things as a hobby. I also enjoy playing
-          the piano, cycling, and exploring ramen restaurants. I like being
-          alone, but I also like creating things with friends. My motto is “just
-          give it a try,” and I value taking on new challenges.
-        </p>
-      </div> */}
     </>
   );
 };
