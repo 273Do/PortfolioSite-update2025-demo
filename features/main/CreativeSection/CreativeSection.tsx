a0095e1c0fd097d6c79626ad0ffd1f3d9b34e65f
273* <keita.y310@icloud.com>
2025-06-19 23:32:51 +0900

    ✨ クリエイティブセクションを実装

---
+++ features/main/CreativeSection/CreativeSection.tsx
@@ -0,0 +1,49 @@
+import ContentScreenLayout from "@/components/ContentScreenLayout";
+import * as AnimationText from "@/components/TextAnimation";
+
+const CreativeSection = async () => {
+  return (
+    <ContentScreenLayout>
+      <div className="w-full">
+        <div className="flex w-full items-center justify-between">
+          <p>Hi, I am 273Do(Tuna-sand). </p>
+          <p>Nice to meet you.</p>
+        </div>
+        <div className="w-4/5">
+          <AnimationText.Reveal className="-mb-12 font-semibold leading-none">
+            CREATIVE
+          </AnimationText.Reveal>
+        </div>
+        <ul className="mt-8">
+          <li>Web Development</li>
+          <li>Fullstack</li>
+          <li>Design</li>
+          <li>UI / UX</li>
+        </ul>
+      </div>
+      <div className="w-full">
+        <ul className="text-end">
+          <li>Sensor Analysis</li>
+          <li>Cloud Platform</li>
+          <li>ML</li>
+        </ul>
+        <div className="flex w-full items-end justify-between">
+          <ul>
+            <li>I develop web apps and tools as a hobby.</li>
+            <li>
+              I am based in the Kansai region and am involved in creative
+              activities.
+            </li>
+          </ul>
+          <div className="w-7/12">
+            <AnimationText.Reveal className="-mb-12 font-semibold leading-none">
+              WORK
+            </AnimationText.Reveal>
+          </div>
+        </div>
+      </div>
+    </ContentScreenLayout>
+  );
+};
+
+export default CreativeSection;
