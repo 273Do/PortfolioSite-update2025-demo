c2fdbc1530e333b62261f2e34518f65376850eb5
273* <keita.y310@icloud.com>
2025-06-21 22:54:42 +0900

    ✨ レンダリング方式ごとの画面レイアウトを用意

---
+++ components/ContentScreenLayout/AsyncLayout.tsx
@@ -0,0 +1,19 @@
+const AsyncLayout = async ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={`flex h-[calc(100vh-5rem)] items-end mix-blend-difference ${className}`}
+    >
+      <div className="flex size-full flex-col items-end justify-between pt-15">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default AsyncLayout;
--- features/main/HeroSection/HeroSection.tsx
+++ features/main/HeroSection/HeroSection.tsx
@@ -1,14 +1,14 @@
-import ContentScreenLayout from "@/components/ContentScreenLayout";
+import AsyncLayout from "@/components/ContentScreenLayout/AsyncLayout";
 import * as AnimationText from "@/components/TextAnimation";
 
 const HeroSection = async () => {
   return (
-    <ContentScreenLayout>
+    <AsyncLayout>
       <p className="right-0 mt-1">Scroll</p>
       <AnimationText.Reveal className="-mb-12 font-semibold leading-none">
         PORTFOLIO
       </AnimationText.Reveal>
-    </ContentScreenLayout>
+    </AsyncLayout>
   );
 };
 
