df0193862bf7e44828b1cb4366479ba6ecf15a3a
273* <keita.y310@icloud.com>
2025-06-23 00:14:34 +0900

    ✨ スキルエリアの軽微な修正

--- features/main/WorksSection/Skills.tsx
+++ features/main/WorksSection/Skills.tsx
@@ -1,7 +1,8 @@
 const Skills = async () => {
   return (
-    <div>
-      <ul className="mt-10 flex h-30 w-full items-center gap-20 bg-slate-700">
+    <div className="my-10">
+      <p className="text-end">Favorite Technology</p>
+      <ul className="flex h-30 w-full items-center gap-20 bg-slate-700">
         <li>icon</li>
         <li>icon</li>
         <li>icon</li>
