9baac4590897c6f6079f5f0b1d83a2badac27d36
273* <keita.y310@icloud.com>
2025-06-26 22:47:34 +0900

    ✨ アクティビティセクションの画像エリアを実装

--- features/main/ActivitySection/ActivitySection.tsx
+++ features/main/ActivitySection/ActivitySection.tsx
@@ -1,26 +1,32 @@
-import AsyncLayout from "@/components/ContentScreenLayout/AsyncLayout";
+import ImageList from "./ImagesArea";
 import SectionTitle from "./SectionTitle";
 
-const ActivitySection = () => {
+const ActivitySection = async () => {
   return (
-    <AsyncLayout className="bg-slate-800">
-      <div className="w-full">
+    <div className="">
+      <div className="sticky top-0 z-20 w-full bg-background">
+        <div className="h-30"></div>
         <SectionTitle />
-        <div className="mt-4 flex w-full items-end justify-between">
-          <ul>
-            <li>Motion Graphics</li>
-            <li>3DCG</li>
-            <li>PV / MV</li>
-          </ul>
-          <ul className="text-end">
-            <li>Stage Piano</li>
-            <li>MTB</li>
-            <li>Ramen</li>
-            <li>Gadget</li>
-          </ul>
-        </div>
       </div>
-    </AsyncLayout>
+
+      <div className="mt-4 mb-10 flex w-full items-end justify-between">
+        <ul>
+          <li>Motion Graphics</li>
+          <li>3DCG</li>
+          <li>PV / MV</li>
+        </ul>
+        <ul className="text-end">
+          <li>Stage Piano</li>
+          <li>MTB</li>
+          <li>Ramen</li>
+          <li>Gadget</li>
+        </ul>
+      </div>
+      <ImageList />
+      <div className="sticky bottom-0 z-10 w-full bg-background">
+        <div className="h-10"></div>
+      </div>
+    </div>
   );
 };
 
--- features/main/ActivitySection/SectionTitle.tsx
+++ features/main/ActivitySection/SectionTitle.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import { ReactFitty } from "react-fitty";
 import * as AnimationText from "@/components/TextAnimation";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const SectionTitle = () => {
   return (
     <AnimationText.ScrollBlinking
       start="center bottom"
-      className="font-semibold leading-none"
+      className="-mt-12 font-semibold leading-none"
     >
       <ReactFitty>ACTIVITIES</ReactFitty>
     </AnimationText.ScrollBlinking>
