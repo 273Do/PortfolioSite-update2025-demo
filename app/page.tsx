86ee5abd70f5d1e6f090a8af71ab3b161fd87d9a
273* <keita.y310@icloud.com>
2025-06-30 23:39:18 +0900

    🩹 ポインターイベントの範囲の変更

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import * as Main from "@/features/main";
 
 export default async function Home() {
   return (
-    <>
+    <div className="pointer-events-none">
       <Main.HeroSection />
       <Main.NoticeSection />
       <Main.CreativeSection />
       <Main.WorksSection />
       <Main.ActivitySection />
-    </>
+    </div>
   );
 }
